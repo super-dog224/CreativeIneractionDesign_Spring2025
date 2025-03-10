b2fd69c1a8119c5c2fa9aa8e8a5076dc381484d6
super-dog224 <2869964346@qq.com>
2025-03-10 16:44:27 +0800

    exercise6.html

---
+++ script.js
@@ -0,0 +1,19 @@
+  document.write('Hello world');
+
+  function sayHello(){
+    var response=prompt('What is your name');
+    alter("Hello"+response+",welcome to the game!")
+  };
+  function add(a,b){
+  return a + b;
+}
+console.log(add(20,20));
+console.log(add(158,20078));
+
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize ="25px";
+  x.style.color='red';
+}
+
+  //sayHellow()
